74b2e9fd787f13caadf3d9b584e068247000ca65
naren9 <knaren9@gmail.com>
2015-06-28 11:56:36 -0700

    Revert "abcd"

--- simple.sh
+++ simple.sh
@@ -2,9 +2,3 @@ date
 time
 echo -e "\n Narendar Reddy"
 echo -e "\n testing branch at append in the file"
-echo -e "test jenkins one"
-echo -e "using for git"
-echo -e "vision"
-echo -e "test"
-echo -e "working fine added"
-echo -e "Checkings of "
